ac39cea0fcf0cb419d583f497bf3a3c47125564c
abhii121 <abishek000as@gmail.com>
2022-04-22 12:33:01 +0530

    task4C#

---
+++ Sudo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpconcepts
+{
+
+		class GFG
+		{
+            public string? name;
+            public string? subject;
+			public void readers(string name, string subject)
+			{
+				this.name = name;
+				this.subject = subject;
+				Console.WriteLine("Myself: " + name);
+				Console.WriteLine("My Favorite Subject is: " + subject);
+			}
+		}
+    class DLithe : GFG
+		{
+			public DLithe()
+			{
+				Console.WriteLine("Dlithe Consultancy");
+			}
+		}
+
+		// Driver class
+		class Sudo
+		{
+        static void Main(string[] args)
+			{
+				DLithe g = new DLithe();
+				g.readers("Abhi", "C#");
+			}
+		}
+}
+
+
---
+++ electricitybill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpconcepts
+{
+    internal class electricitybill
+    {
+
+        public abstract class GST_Calculation
+        {
+            protected double Bill;
+            public abstract void GetBill();
+            public void Calculate_GST(double GST)
+            {
+                Console.WriteLine("Electricity Bill is:{0}", Bill + GST);
+            }
+        }
+        public class ebill : GST_Calculation
+        {
+            public override void GetBill()
+            {
+                Bill = 1200;
+            }
+
+        }
+      
+        public class Abstraction
+        {
+            public static void Main()
+            {
+                GST_Calculation calc;
+                calc = new ebill();
+                calc.GetBill();
+                calc.Calculate_GST(240);
+                
+            }
+        }
+    }
+       
+}
+
